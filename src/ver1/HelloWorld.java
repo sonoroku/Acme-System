b7436c2ef94e77153983a8a9a4eb1c1ab7bbcb69
Alex ORourke <aforourke888@gmail.com>
2026-06-21 15:20:26 -0500

    Added branch work message

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -3,5 +3,6 @@ package ver1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from IntelliJ");
+        System.out.println("Alex is working locally on his branch");
     }
 }

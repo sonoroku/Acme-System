8342e3f44b1620057a19eea306ef169a9733e97d
Alex ORourke <aforourke888@gmail.com>
2026-06-21 16:40:49 -0500

    added whatFor method

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -5,11 +5,17 @@ public class HelloWorld {
         System.out.println("Hello from IntelliJ");
         System.out.println("Alex is working locally on his branch");
         heyNow();
+        whatFor("Coffee");
     }
 
     public static void heyNow() {
         String Msg = "Hey Now!";
         System.out.println(Msg);
+
+    }
+
+    public static void whatFor(String stringArg) {
+        System.out.println(stringArg + ", what for?");
     }
 
 }
